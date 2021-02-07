81663eeae3c89bdece6bc8e570677fd42fd16510
philip.a.markgraf <philip_markgraf@mac.com>
2021-02-06 17:09:30 -0800

    f added capacity one :: is_full

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
-struct CircularBuffer {}
+struct CircularBuffer {
+    capacity: usize,
+    num_elem: usize,
+}
 
 impl CircularBuffer {
-    fn new(_:usize) -> CircularBuffer {
-        CircularBuffer {}
+    fn new(c: usize) -> CircularBuffer {
+        CircularBuffer { capacity: c, num_elem: 0}
     }
     fn is_empty(&self) -> bool { true }
-    fn is_full(&self) -> bool { true }
+    fn is_full(&self) -> bool { self.num_elem >= self.capacity }
     fn size(&self) -> usize { 0 }
     fn put(&self, _: i32) -> bool { false }
     fn get(&self) -> i32 { i32::MIN }
@@ -32,8 +35,9 @@ mod tests {
         assert_eq!(i32::MIN, b.get());
     }
     #[test]
-    fn when_create_capacity_one_then_is_empty_true_size_zero() {
-        let b = CircularBuffer::new(0);
+    fn when_create_capacity_one_then_is_full_false_is_empty_true_size_zero() {
+        let b = CircularBuffer::new(1);
+        assert_eq!(false, b.is_full());
         assert!(b.is_empty());
         assert_eq!(0, b.size());
     }
