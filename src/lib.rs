0d61ee04a8221729fa58ddb5c9bfe1c7b5ea0c35
philip.a.markgraf <philip_markgraf@mac.com>
2021-02-06 16:41:12 -0800

    f added size

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,17 @@ impl CircularBuffer {
     }
     fn is_empty(&self) -> bool { true }
     fn is_full(&self) -> bool { true }
+    fn size(&self) -> usize { 0 }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn when_create_capacity_zero_then_is_full_true_is_empty_true() {
+    fn when_create_capacity_zero_then_is_full_true_is_empty_true_size_zero() {
         let b = CircularBuffer::new(0);
         assert!(b.is_full());
-        assert!(b.is_empty())
+        assert!(b.is_empty());
+        assert_eq!(0, b.size());
     }
 }
